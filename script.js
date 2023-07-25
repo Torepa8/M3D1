fe6b57ae439840c2bab105ed6df055d138b6225a
Torepa8 <65725642+Torepa8@users.noreply.github.com>
2023-07-26 00:02:11 +0200

    ok

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-// alert("Ciao")
-const twitter=document.querySelector(".p-4:last-of-type li:nth-of-type(2) a")
-twitter.remove()
+// EXTRA Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automticamente.
+    const twitter=document.querySelector(".p-4:last-of-type li:nth-of-type(2) a")
+    twitter.remove()
 
-const padre=document.querySelector("div.container")
 
-const autori=document.querySelector("div.blog-post p.blog-post-meta a")
+
+
+
+// EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
+
+const contlegg=document.querySelectorAll(".col-md-6 a")
+for (const c of contlegg) {    
+    c.addEventListener("click", function(event){
+        event.target.closest(".col-md-6").remove()
+        // event.target.closest(".container").remove()
+    })
+}
 
 // autori.addEventListener("onmouseover", function(){
 //     alert(autori.st)})
+// EXTRA Crea con JavaScript la funzionalità per creare un alert col nome dell'autore ogni volta che il cursore passa sopra l'autore del post.
 
+const autori=document.querySelectorAll("div.blog-post p.blog-post-meta a")
+for (const autore of autori) {    
+    autore.addEventListener("onmouseover", function(event){
+        alert("Elimino il padre corrispondente all'autore che clicco che è "+event.target.innerText)
+        event.target.closest("div.blog-post").remove()
+    })
+}
