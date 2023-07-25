cb2957b85ca0bcf520109471312d35b8389da373
Torepa8 <65725642+Torepa8@users.noreply.github.com>
2023-07-25 23:12:15 +0200

    agg

---
+++ script.js
@@ -0,0 +1,11 @@
+// alert("Ciao")
+const twitter=document.querySelector(".p-4:last-of-type li:nth-of-type(2) a")
+twitter.remove()
+
+const padre=document.querySelector("div.container")
+
+const autori=document.querySelector("div.blog-post p.blog-post-meta a")
+
+// autori.addEventListener("onmouseover", function(){
+//     alert(autori.st)})
+
