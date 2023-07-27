9cf45b06a095fc150eca1b796eba88d57c43b8e7
Torepa8 <65725642+Torepa8@users.noreply.github.com>
2023-07-27 12:59:16 +0200

    commit da terminale

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const twitter=document.querySelector(".p-4:last-of-type li:nth-of-type(2) a")
 
 
 //prova commit
+//prova commit 2
 
 // EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
 
